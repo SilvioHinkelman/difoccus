5df072c87002a5f485c60d799c6c1c32f06740db
SilvioHinkelman <73371769+SilvioHinkelman@users.noreply.github.com>
2025-04-25 14:12:16 -0300

    Install MUI

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,27 +1,23 @@
 "use client";
-
+import { Button, Drawer as DawerComp } from "@mui/material";
 import { useState } from "react";
 
 const Drawer = () => {
   const [open, setOpen] = useState(false);
   console.log("TCL: Drawer -> open", open);
   return (
-    <>
-      <button
-        onClick={() => {
-          setOpen(!open);
-        }}
-      >
-        Clique me
-      </button>
-      <div
-        className={`${
-          open ? "absolute top-0 right-0 bg-red-600 w-36" : "hidden"
-        }`}
-      >
-        <h1>TEfa</h1>
-      </div>
-    </>
+    <div className="p-8">
+      <Button variant="contained" onClick={() => setOpen(true)}>
+        Abrir Drawer
+      </Button>
+
+      <DawerComp anchor="right" open={open} onClose={() => setOpen(false)}>
+        <div style={{ width: 250, padding: 20 }}>
+          <h2>Conteúdo do Drawer</h2>
+          <p>Algum texto aqui dentro.</p>
+        </div>
+      </DawerComp>
+    </div>
   );
 };
 export default Drawer;
