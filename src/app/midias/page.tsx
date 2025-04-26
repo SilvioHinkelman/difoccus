aa155ec93a46661e0c2bb5e4d487fef7f1d12c58
SilvioHinkelman <73371769+SilvioHinkelman@users.noreply.github.com>
2025-04-26 10:18:01 -0300

    bug:swiper

--- src/app/midias/page.tsx
+++ src/app/midias/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import { MockSwiper, ImageType } from "../../constants/mockSwiper";
 import { Swiper, SwiperSlide, SwiperProps } from "swiper/react";
 import { EffectCreative, Autoplay } from "swiper/modules";
 import "swiper/css";
@@ -31,16 +30,15 @@ const Players: React.FC = () => {
   };
 
   return (
-    <Swiper {...config}
-     className="w-full amax-w-2xl"
-    >
-      {MockSwiper.map((image: ImageType, index: number) => (
+    <Swiper {...config} className="w-full amax-w-2xl">
+      {Array.from({ length: 29 }, (_, index: number) => (
         <SwiperSlide key={index}>
-          <div className="w-full h-[1280px] bg-[#131524] flex items-center justify-center rounded-lg overflow-hidden">
-          <Image
-              alt="img"
-              src={image.img}
-              width={1280}
+          <div className="w-full h-[680px] bg-[#131524] flex items-center justify-center rounded-lg overflow-hidden">
+            <Image
+              alt={`photo-${index}`}
+              src={`/album/photo-${index}.JPG`}
+              width={1920}
+              height={1280}
               priority={index === 0}
               className="max-w-full max-h-full object-contain"
             />
